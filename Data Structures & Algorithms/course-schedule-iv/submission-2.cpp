daaf1c527c5ce45690a36a3fe320ca2a2950d69a
yashatan <58113035+yashatan@users.noreply.github.com>
2026-04-22 11:35:15 +0700

    Add: course-schedule-iv - submission-2

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-2.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+        unordered_map<int, set<int>> children;
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+
+        if (prerequisites.size() ==0){
+            return vector<bool> (queries.size(), false);
+        }
+        unordered_map<int, vector<int>> adj;
+
+        for (int i=0; i < numCourses; i++){
+            adj[i] = vector<int>();
+            children[i] = set<int>();
+        }
+
+        for (auto prere : prerequisites){
+            int a = prere[0];
+            int b = prere[1];
+            adj[a].push_back(b);
+        }
+        
+        unordered_set<int> visited;
+        unordered_set<int> path;
+
+        vector<int> topsort;
+
+        for (int i=0; i < numCourses; i++){
+            dfs(i, adj, visited, topsort, children[i]);
+        }
+        reverse(topsort.begin(), topsort.end());
+
+        vector<bool> res;
+
+        for(auto querie : queries){
+            int first = querie[0];
+            int second = querie[1];
+            if (children[first].count(second) >0){
+               res.push_back(true);
+            }else{
+                res.push_back(false);
+            }
+
+        }
+        return res;
+    }
+
+    void dfs(int src, unordered_map<int, vector<int>>& adj, unordered_set<int>& visit, vector<int>& topSort, set<int>& childrens){
+        if (visit.count(src) > 0) {
+           return;
+        }
+
+        visit.insert(src);
+        for (int neighbor : adj[src]) {
+            childrens.insert(neighbor);
+            dfs(neighbor, adj, visit, topSort, children[neighbor]);
+            for(int childrenes : children[neighbor]){
+                childrens.insert(childrenes);
+            }
+        }
+
+        topSort.push_back(src);
+    }
+};
