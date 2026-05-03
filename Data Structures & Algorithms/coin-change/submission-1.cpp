9b45fa51bca1a9185ad0d32e0d14b41d17a8280f
yashatan <58113035+yashatan@users.noreply.github.com>
2026-05-03 14:31:55 +0700

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        std::sort(coins.begin(), coins.end(), std::greater<int>());
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0] =0;
+        for (int a=coins[0]; a <= amount; a += coins[0]){
+            dp[a] = dp[a-coins[0]] + 1;
+        }
+
+        cout << "dp = [ ";
+        for(int dpa : dp){
+            cout << dpa <<", ";
+        }
+        cout << "]" << endl;
+        for(int i =1; i< coins.size(); i++){
+            vector<int> curRow(amount+1, INT_MAX);
+            int curCoin = coins[i];
+            for(int a = 0; a<= amount; a++){
+                if(curCoin > a){
+                    curRow[a] = dp[a];
+                    continue;
+                }
+                
+                if( a >= curCoin){
+                    int skip = dp[a];
+                    int include = skip;
+                    if (curRow[a-curCoin] != INT_MAX){
+                        include = curRow[a-curCoin] + 1;
+                    }
+                    curRow[a] = min(include, skip);
+                }
+            }
+            dp = curRow;
+                    cout << "dp = [ ";
+        for(int dpa : dp){
+            cout << dpa <<", ";
+        }
+        cout << "]" << endl;
+        }
+
+        return dp[amount]==INT_MAX?-1:dp[amount];
+    }
+};
+
+
