9e352359228fc37390a0a6b5ca969424e093e7c7
yashatan <58113035+yashatan@users.noreply.github.com>
2026-05-05 12:01:05 +0700

    Add: minimum-cost-for-tickets - submission-1

---
+++ Data Structures & Algorithms/minimum-cost-for-tickets/submission-1.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        vector<int> dp(days.size(), 0);
+
+        int oneDayPrice = costs[0];
+        int sevenDayPrice = costs[1];
+        int thirdtyDayPrice = costs[2];
+
+        dp[0] = oneDayPrice;
+        for(int i =1; i < days.size(); i++){
+            dp[i] = dp[i-1]+oneDayPrice;
+        }
+
+        vector<int> curRow(days.size(), 0);
+        int L7=0, L30 =0;
+        for(int i =0; i < days.size(); i++){
+            // if (days[i] <7){
+            //     curRow[i] = dp[i];
+            //     continue;
+            // } 
+            while(days[i]-days[L7] > 6){
+                L7++;
+            }
+            int takeOld = dp[i];
+            int takeSeven = 0;
+            if (L7 >0) {takeSeven = curRow[L7-1]+sevenDayPrice;}
+            else{
+                takeSeven = sevenDayPrice; 
+            }
+            int takeOne = sevenDayPrice;
+            if(i!=0) takeOne = curRow[i-1] +oneDayPrice;
+            curRow[i] = min(takeOld, min(takeSeven,takeOne));
+        }
+        dp = curRow;
+
+        curRow = vector<int>(days.size(), 0);
+        L7 =0;
+        for(int i =0; i < days.size(); i++){
+            // if (days[i] <30){
+            //     curRow[i] = dp[i];
+            //     continue;
+            // } 
+            while(days[i]-days[L7] > 6){
+                L7++;
+            }
+
+            while(days[i]-days[L30] > 29){
+                L30++;
+            }
+            int takeOld = dp[i];
+
+            int takeSeven = 0;
+            if (L7 >0) {takeSeven = curRow[L7-1]+sevenDayPrice;}
+            else{
+                takeSeven = sevenDayPrice; 
+            }
+            int takeNewOne = thirdtyDayPrice;
+
+            int takeThirty = 0;
+
+            if (L30 >0) {takeThirty = curRow[L30-1]+thirdtyDayPrice;}
+            else{
+                takeThirty = thirdtyDayPrice; 
+            }
+
+
+
+            if(i!=0) takeNewOne = curRow[i-1] +oneDayPrice;
+
+
+
+            curRow[i] = min({takeOld,takeSeven,takeNewOne, takeThirty});
+        }
+
+        dp = curRow;
+        return dp[days.size()-1];
+    }
+};
