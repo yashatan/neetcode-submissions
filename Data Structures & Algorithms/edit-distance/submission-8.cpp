0aef4c26efda464b071116cc988160109bd2bc90
yashatan <58113035+yashatan@users.noreply.github.com>
2026-05-14 15:16:52 +0700

    Add: edit-distance - submission-8

---
+++ Data Structures & Algorithms/edit-distance/submission-8.cpp
@@ -0,0 +1,114 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int res =0;
+        vector<int> dp (word1.length(), 0);
+        bool initFoundWord = false;
+
+        if (word1.length() ==0 || word2.length()==0){
+            return max(word1.length(),word2.length());
+        }
+
+        if(word1[0] == word2[0]){
+            dp[0] = 0;
+            initFoundWord = true;
+        }else{
+            dp[0] = 1;
+        }
+
+
+        for (int initi =1; initi < dp.size(); initi++){
+            if(word1[initi] == word2[0] && !initFoundWord){
+                dp[initi] = dp[initi-1];
+                initFoundWord= true;
+            }else{
+                dp[initi] = dp[initi-1]+1;
+            }
+        }
+            cout << "dp = [";   
+            for (int j =0 ; j < dp.size(); j++){
+                cout << dp[j] << ", ";
+            }
+            cout << "]" <<endl;
+        for(int i2 = 1; i2 < word2.size(); i2++){
+            vector<int> curRow (word1.length(), 0);
+            if(word1[0] == word2[i2]){
+                curRow[0] = dp[0];
+            }else{
+                curRow[0] = dp[0]+1;
+            }
+            // bool foundWord = false;
+    
+            // for(int i1 = 0; i1 < word1.size(); i1++){
+            //     if(i1 < i2 -1){
+            //         if (word1[i1] == word2[i2]){
+            //             if(i1 == 0){
+            //                 curRow[i1] = dp[i1];
+            //             }else{
+            //                 curRow[i1] = dp[i1-1];
+            //             }
+                        
+            //         }else{
+            //             if(i1 == 0){
+            //                 curRow[i1] = dp[i1] +1;
+            //             }else{
+            //                 curRow[i1] = min(dp[i1] +1, curRow[i1-1]+1);
+            //             }
+
+            //         }
+
+            //     }
+
+            //     if (i1 == i2-1){
+
+            //             if (word1[i1] == word2[i2]){
+            //                 if(i1 ==0){
+            //                     curRow[i1] = dp[i1];
+            //                     //foundWord = true;
+            //                 }else{
+            //                     curRow[i1] = dp[i1-1];
+            //                 }
+
+            //             }else{
+            //                 curRow[i1] = dp[i1] +1;
+            //             }
+                    
+
+            //     }
+
+            //     if (i1 == i2){
+            //         if (word1[i1] == word2[i2]){
+            //             curRow[i1] = dp[i1-1];
+            //             foundWord = true;
+            //         }else{
+            //             curRow[i1] = min({dp[i1-1] +1, curRow[i1-1]+1, dp[i1] +1});
+            //         }
+            //     }
+
+            //     if(i1>i2){
+            //         if (word1[i1] == word2[i2] && !foundWord){
+            //             curRow[i1] = dp[i1-1];
+            //             foundWord = true;
+            //         }else{
+            //             curRow[i1] = curRow[i1-1]+1;
+            //         }
+            //     }
+                for(int i1 = 1; i1 < word1.size(); i1++){
+                    if(word1[i1] == word2[i2]){
+                        curRow[i1] = dp[i1-1];
+                    }else{
+                        curRow[i1] = min({dp[i1]+1, curRow[i1-1]+1, dp[i1-1]+1});
+                    }
+                }
+            
+            dp = curRow;
+            cout << "dp = [";   
+            for (int j =0 ; j < dp.size(); j++){
+                cout << dp[j] << ", ";
+            }
+            cout << "]" <<endl;
+        }
+
+        return dp[word1.size()-1];
+    }
+};
