af24ae1a10e27a1cdfb20ef62330c1e452a33868
yashatan <58113035+yashatan@users.noreply.github.com>
2026-05-22 17:45:34 +0700

    Add: longest-palindromic-substring - submission-5

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+	string res ="";
+	string longestPalindrome(string s) {
+		for (int i = 0; i < s.length(); i++) {
+			int tempLength = 0;
+			string temp = dfs(i, i, s);
+			if (res.length() < temp.length()) { 
+				res = temp; }
+		}
+
+		for (int i = 0; i < s.length()-1; i++) {
+			int tempLength = 0;
+			string temp = dfs(i, i+1, s);
+			if (res.length() < temp.length()) { 
+				res = temp; }
+		}
+
+		return res;
+	}
+
+	string dfs(int L, int R, string& s) {
+		string resTemp;
+		while (L >= 0 && R < s.length() && s[L] == s[R]) {
+			resTemp = s.substr(L, R-L+1);
+			L--;
+			R++;
+		}
+		return resTemp;
+	}
+};
