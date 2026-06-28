7531f565d456d4cf901ea0817459c801cb6c57a3
yashatan <58113035+yashatan@users.noreply.github.com>
2026-06-28 18:01:26 +0700

    Add: subtree-of-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-3.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+
+        if (!root && !subRoot) {
+            return true;
+        }
+
+        return checkNormal(root, subRoot);
+    }
+
+    bool checkNormal(TreeNode* root, TreeNode* subRoot) {
+
+        if ((!root && subRoot) || (root && !subRoot)) {
+            return false;
+        }
+        cout << root->val << endl;
+        if (root->val != subRoot->val) {
+            return checkNormal(root->left, subRoot) || checkNormal(root->right, subRoot);
+        }
+        else {
+            if (checkEqual(root, subRoot)) {
+                return true;
+            }
+            else {
+                return checkNormal(root->left, subRoot) || checkNormal(root->right, subRoot);
+            }
+        }
+    }
+
+
+    bool checkEqual(TreeNode* root, TreeNode* subRoot) {
+
+        if (!root && !subRoot) {
+            return true;
+        }
+        
+        if ((!root && subRoot) || (root && !subRoot)) {
+            return false;
+        }
+
+
+        cout << endl <<"check equal" << endl;
+        cout << "root: " << root->val << " subRoot: " << subRoot->val << endl;
+        if (root->val != subRoot->val) {
+            return false;
+        }
+
+        if ((!root->left && !root->right && !subRoot->left && !subRoot->right)) {
+            return true;
+        }
+        return checkEqual(root->left, subRoot->left) && checkEqual(root->right, subRoot->right);
+    }
+};
