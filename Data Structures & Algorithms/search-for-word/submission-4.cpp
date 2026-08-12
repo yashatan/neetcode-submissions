597fc886df0d2c2f36956bdc3c0dbbbec50891ea
yashatan <58113035+yashatan@users.noreply.github.com>
2026-08-12 23:22:12 +0700

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.cpp
@@ -0,0 +1,56 @@
+class Solution {
+   public:
+    int row, col = 0;
+        set<pair<int, int>> path;
+    bool exist(vector<vector<char>>& board, string word) {
+        row = board.size();
+        col = board[0].size();
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (board[r][c] == word[0]) {
+                    vector<vector<bool>> visit(row, std::vector<bool>(col, 0));
+                    cout << "found first char " << endl;
+                    if (backtrack(board, word, 0, r, c)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    bool backtrack(vector<vector<char>>& board, string word, int k,                   int r, int c) {
+        if (k == word.length()) {
+            return true;
+        }
+
+        if (r < 0 || r >= row) {
+            return false;
+        }
+
+        if (c < 0 || c >= col) {
+            return false;
+        }
+
+        if ( path.count({r, c})) {
+            return false;
+        }
+        char curChar = board[r][c];
+
+        if (curChar != word[k]) {
+            return false;
+        }
+
+
+        cout << "valid char: " << curChar << endl;
+        cout << "r: " << r << " c: " << c << endl;
+        path.insert({r, c});
+        bool res =  (backtrack(board, word, k + 1, r - 1, c) ||
+                backtrack(board, word, k + 1, r, c - 1) ||
+                backtrack(board, word, k + 1, r + 1, c) || backtrack(board, word, k + 1, r, c + 1));
+        path.erase({r, c});
+        return res;
+
+    }
+};
