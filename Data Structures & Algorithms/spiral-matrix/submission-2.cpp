298f7a2446200657a84423b92c39a742689031a7
yashatan <58113035+yashatan@users.noreply.github.com>
2026-09-17 15:35:08 +0700

    Add: spiral-matrix - submission-2

---
+++ Data Structures & Algorithms/spiral-matrix/submission-2.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.size() == 0) {
+            return vector<int>();
+        }
+        int rowToGo = matrix.size();
+        int colToGo = matrix[0].size();
+
+        vector<int> res;
+        bool trueGoUpFalseGoDown = false;
+        bool trueGoLeftFalseGoRight = false;
+        bool trueHorFalseVer = true;
+
+        int row = 0;
+        int col = -1;
+
+        while (rowToGo > 0 && colToGo > 0) {
+            // Go Horizon
+
+            if (trueHorFalseVer) {
+                for (int i = 0; i < colToGo; i++) {
+                    if (!trueGoLeftFalseGoRight) {
+                        col++;
+                    }
+                    else {
+                        col--;
+                    }
+                    res.push_back(matrix[row][col]);
+                }
+                rowToGo--;
+                trueGoLeftFalseGoRight = !trueGoLeftFalseGoRight;
+                trueHorFalseVer = !trueHorFalseVer;
+            }
+            else {
+                for (int i = 0; i < rowToGo; i++) {
+                    if (!trueGoUpFalseGoDown) {
+                        row++;
+                    }
+                    else {
+                        row--;
+                    }
+                    res.push_back(matrix[row][col]);
+                }
+                colToGo--;
+                trueGoUpFalseGoDown = !trueGoUpFalseGoDown;
+                trueHorFalseVer = !trueHorFalseVer;
+            }
+        }
+        return res;
+    }
+};
