9cedd8a0cb12249ebc86a213380c3148e5ba055b
yashatan <58113035+yashatan@users.noreply.github.com>
2026-09-19 21:31:41 +0700

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> cache;
+    int uniquePaths(int m, int n) {
+        cache = vector<vector<int>>(m, vector<int>(n,0));
+        return countPath(0,0,m,n);
+    }
+
+    int countPath(int r, int c, int rows, int cols){
+        if(r == rows || c == cols){
+            return 0;
+        }
+        if (cache[r][c] != 0){
+            return cache[r][c];
+        }
+        if (r == rows-1 && c == cols -1){
+            return 1;
+        }
+
+        cache[r][c] = countPath(r+1, c, rows, cols) + countPath(r,c+1,rows,cols);
+                cout << "cache" << endl;
+        for(auto row : cache){
+            for (auto value : row){
+                cout << value << " ";
+
+            }
+        cout << endl;
+        }
+        return cache[r][c];
+    }
+};
